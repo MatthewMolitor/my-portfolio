567926c9db975126d85d8407281d3c18c044212c
Molitor, Matthew E <matthew.molitor@wsu.edu>
2022-02-14 15:43:20 -0800

    added links to pokemon and reddit projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,10 +6,19 @@ function Projects(props) {
     
        return (
 <>
-      <div className='App-body'>
-        <div className='App-sub-body'>
+      <div className='projects-body'>
+        <div className='projects-body'>
           <p className='centered'>
             These are my Projects
+          </p>
+          <p className='centered'>
+            <a href="https://colab.research.google.com/drive/1tlmd4mRJKPJX922NeIkDGVYxe6xKX35G?usp=sharing">Pokemon Team builder:</a>
+            used data scraped from teams used in tournaments to create 
+            suggestions for entirely new teams that can be applied to different competitive formats
+          </p>
+          <p className='centered'>
+            <a href={process.env.PUBLIC_URL + 'images/reddit_prediction.pdf'}>Reddit:</a>
+            Parsed ban data from several subreddits to build a binary ban predictor for reddit posts.
           </p>
         </div>
       </div>
